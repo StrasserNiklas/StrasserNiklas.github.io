bf06f06d9a7973b5a4e4fb627bbf94b2f44f297d
NiklasStrasser <niklasstrasser@hotmail.com>
2026-01-30 16:22:01 +0100

    fix with buddies maxi

--- script.js
+++ script.js
@@ -23855,11 +23855,11 @@ document.getElementById('toggleButton-buddies-maxi').addEventListener('click', f
     if (withBuddies) {
       var button = document.getElementById('toggleButton-buddies-maxi');
       button.textContent = "Alle";
-      displayActivities(activitiesmaxi.filter(entry => entry.buddies.length > 0), 'activity-list-maxi', 'h1-maxi', 'maxi');
+      displayActivities(activitiesMaxi.filter(entry => entry.buddies.length > 0), 'activity-list-maxi', 'h1-maxi', 'maxi');
     } else {
       var button = document.getElementById('toggleButton-buddies-maxi');
       button.textContent = "Mit Buddies";
-      displayActivities(activitiesmaxi, 'activity-list-maxi', 'h1-maxi', 'Maxi');
+      displayActivities(activitiesMaxi, 'activity-list-maxi', 'h1-maxi', 'Maxi');
     }
 });
 
